36b25eb072e5f1641c0bdda7d6e0b2d3f7118e51
verstatx <ayer.3d@gmail.com>
2024-09-25 15:12:46 -0400

    Update and swap back to upstream ASR.
    Updated to use new/stabilized APIs.
    Scrap the custom SceneManager.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use asr::{async_main, Address, dotnet::{Mono, MonoVersion/*, SceneManager*/}, Error, future::next_tick, Process, string::{ArrayString}, timer, timer::TimerState, watcher::Watcher};
+use asr::{async_main, Address, game_engine::unity::{mono::Module, SceneManager, get_scene_name}, Error, future::{retry, next_tick}, PointerSize, Process, settings::Gui, string::{ArrayString}, timer, timer::TimerState, watcher::Watcher};
 use bytemuck::CheckedBitPattern;
 use derive;
 
 static PROCESS_NAME : &str = "Risk of Rain 2.exe";
 
-#[derive(asr::Settings)]
+#[derive(Gui)]
 struct AutoSplitterSettings {
     /// Allow the autosplitter to start the timer automatically
     #[default = true]
@@ -17,7 +17,7 @@ struct AutoSplitterSettings {
     reset: bool,
 }
 
-#[derive(asr::Settings)]
+#[derive(Gui)]
 struct GameSettings {
     /// Split when leaving Bazaar Between Time
     #[default = true]
@@ -61,7 +61,7 @@ struct StaticField<'a> {
 
 impl StaticField<'_> {
     fn read_value<T: CheckedBitPattern>(&self) -> Result<T, Error> {
-        return self.process.read_pointer_path64::<T>(self.base_address, &[0, self.field_offset]);
+        return self.process.read_pointer_path::<T>(self.base_address, PointerSize::Bit64, &[0, self.field_offset]);
     }
 }
 
@@ -182,25 +182,26 @@ async fn main() {
     loop {
         let process = Process::wait_attach(process_name).await;
         process.until_closes(async {
-            let monomod = Mono::wait_attach(&process, MonoVersion::MonoV2).await;
-            let sceneman = SceneManager::wait_new(&process).await;
+            //let monomod = Module::wait_attach(&process, asr::game_engine::unity::mono::Version::V3).await;
+            let monomod = Module::wait_attach_auto_detect(&process).await;
+            let sceneman = SceneManager::wait_attach(&process).await;
 
             // Workaround for version detection: wait until the scene is valid
             // before attempting to load RoR2.dll/Assembly-CSharp.dll
             // FIXME replace with file check + wait_get_image
-            retry(|| sceneman.get_current_scene_name::<16>()).await;
+            retry(|| sceneman.get_current_scene_path::<256>(&process)).await;
 
             // SotV onwards uses RoR2.dll, earlier versions use Assembly-CSharp.dll
             // FIXME breaks version assumption if RoR2.dll has not yet loaded
             // check if file "RoR2.dll" exists once wasi support is merged.
-            if let Some(ror2) = monomod.get_image("RoR2").or(monomod.get_default_image()) {
+            if let Some(ror2) = monomod.get_image(&process, "RoR2").or(monomod.get_default_image(&process)) {
 
                 // FadeToBlackManager exists almost at the start of the process, but starts off invalid
-                let mut ftbm = ror2.get_class("FadeToBlackManager");
+                let mut ftbm = ror2.get_class(&process, &monomod, "FadeToBlackManager");
                 // Run exists from entering the lobby onwards
-                let mut run = ror2.get_class("Run");
+                let mut run = ror2.get_class(&process, &monomod, "Run");
                 // GameOverController exists just before the end of a run, including dying
-                let mut goc = ror2.get_class("GameOverController");
+                let mut goc = ror2.get_class(&process, &monomod, "GameOverController");
                 // alpha valid when FadeToBlackManager exists
                 let mut alpha_loc : Option<Address> = None;
                 // stageClearCount only valid during a run (not valid in the lobby)
@@ -214,50 +215,50 @@ async fn main() {
                 loop {
                     // attmept to reload class fields when invalid
                     if ftbm.is_none() {
-                        ftbm = ror2.get_class("FadeToBlackManager");
+                        ftbm = ror2.get_class(&process, &monomod, "FadeToBlackManager");
                         alpha_loc = None;
                     }
 
                     if run.is_none() {
-                        run = ror2.get_class("Run");
+                        run = ror2.get_class(&process, &monomod, "Run");
                         stage_loc = None;
                     }
 
                     if goc.is_none() {
-                        goc = ror2.get_class("GameOverController");
+                        goc = ror2.get_class(&process, &monomod, "GameOverController");
                         panel_loc = None;
                     }
 
                     if let Some(ftbm) = ftbm.as_ref() {
                         if alpha_loc.is_none() {
-                            let alpha_offset = ftbm.get_field("alpha");
-                            let alpha_addr = ftbm.get_static_table();
+                            let alpha_offset = ftbm.get_field_offset(&process, &monomod, "alpha");
+                            let alpha_addr = ftbm.get_static_table(&process, &monomod);
                             if let (Some(alpha_offset), Some(alpha_addr)) = (alpha_offset, alpha_addr) {
-                                alpha_loc = Some(alpha_addr.add(alpha_offset));
+                                alpha_loc = Some(alpha_addr.add(alpha_offset.into()));
                             }
                         }
                     }
 
                     if let Some(run) = run.as_ref() {
                         if stage_loc.is_none() {
-                            let instance_field = run.get_field("<instance>k__BackingField");
-                            let scc_field = run.get_field("stageClearCount");
-                            let static_table = run.get_static_table();
+                            let instance_field = run.get_field_offset(&process, &monomod, "<instance>k__BackingField");
+                            let scc_field = run.get_field_offset(&process, &monomod, "stageClearCount");
+                            let static_table = run.get_static_table(&process, &monomod);
                             if let (Some(instance_field), Some(static_table), Some(scc_field)) = (instance_field, static_table, scc_field) {
-                                let instance_addr = static_table.add(instance_field);
-                                stage_loc = Some(StaticField{process: &process, base_address: instance_addr, field_offset: scc_field})
+                                let instance_addr = static_table.add(instance_field.into());
+                                stage_loc = Some(StaticField{process: &process, base_address: instance_addr, field_offset: scc_field.into()})
                             }
                         }
                     }
 
                     if let Some(goc) = goc.as_ref() {
                         if panel_loc.is_none() {
-                            let instance_field = goc.get_field("<instance>k__BackingField");
-                            let sdgerp_field = goc.get_field("<shouldDisplayGameEndReportPanels>k__BackingField");
-                            let static_table = goc.get_static_table();
+                            let instance_field = goc.get_field_offset(&process, &monomod, "<instance>k__BackingField");
+                            let sdgerp_field = goc.get_field_offset(&process, &monomod, "<shouldDisplayGameEndReportPanels>k__BackingField");
+                            let static_table = goc.get_static_table(&process, &monomod);
                             if let (Some(instance_field), Some(static_table), Some(sdgerp_field)) = (instance_field, static_table, sdgerp_field) {
-                                let instance_addr = static_table.add(instance_field);
-                                panel_loc = Some(StaticField{process: &process, base_address: instance_addr, field_offset: sdgerp_field})
+                                let instance_addr = static_table.add(instance_field.into());
+                                panel_loc = Some(StaticField{process: &process, base_address: instance_addr, field_offset: sdgerp_field.into()})
                             }
                         }
                     }
@@ -293,8 +294,9 @@ async fn main() {
 
                     // update the scene name
                     // skip scene name updates during scene transitions (always invalid)
-                    if let Some(scene) = sceneman.get_current_scene_name::<16>().ok() {
-                        state.scene.update(Some(scene));
+                    if let Some(scene) = sceneman.get_current_scene_path::<256>(&process).ok() {
+                        let utf8_scene = std::str::from_utf8(get_scene_name(scene.as_bytes())).unwrap_or_default();
+                        state.scene.update(ArrayString::<16>::from(&utf8_scene).ok());
                     }
 
 
@@ -324,62 +326,4 @@ async fn main() {
         }).await;
     }
 }
-
-/*** TEMPORARY WORKAROUND ***/
-use asr::{
-    future::retry, signature::Signature, Address64, string::ArrayCString,
-};
-
-/// Simplified SceneManager with a Linux workaround.
-pub struct SceneManager<'a> {
-    process: &'a Process,
-    address: Address,
-}
-
-impl<'a> SceneManager<'a> {
-    /// Creates a new instance of `SceneManager`
-    pub fn new(process: &'a Process) -> Option<Self> {
-        const SIG_64_BIT: Signature<13> = Signature::new("48 83 EC 20 4C 8B ?5 ???????? 33 F6");
-
-        let unity_player = process.get_module_address("UnityPlayer.dll").ok()?;
-        let address = SIG_64_BIT.scan_process_range(process, (unity_player, 0x2000000))? + 7;
-        let address = address + 0x4 + process.read::<i32>(address).ok()?;
-
-        Some(Self {process, address})
-    }
-
-    /// Creates a new instance of `SceneManager`
-    pub async fn wait_new(process: &'a Process) -> SceneManager<'_> {
-        retry(|| Self::new(process)).await
-    }
-
-    fn read_pointer(&self, address: Address) -> Result<Address, Error> {
-        Ok(self.process.read::<Address64>(address)?.into())
-    }
-
-    fn get_current_scene_address(&self) -> Result<Address, Error> {
-        const ACTIVE_SCENE : u32 = 0x48;
-        let addr = self.read_pointer(self.address)?;
-        self.read_pointer(addr + ACTIVE_SCENE)
-    }
-
-    /// Returns the name associated with the current scene
-    pub fn get_current_scene_name<const N: usize>(&self) -> Result<ArrayString<N>, Error> {
-        const ASSET_PATH : u32 = 0x10;
-
-        let addr = self.read_pointer(self.get_current_scene_address()? + ASSET_PATH)?;
-        let path = self.process.read::<ArrayCString<256>>(addr)?;
-        let Some(name) = path.split(|&b| b == b'/').last() else { return Err(self.process.get_module_address("Error").unwrap_err()) };
-        let Some(name) = name.split(|&b| b == b'.').next() else { return Err(self.process.get_module_address("Error").unwrap_err()) };
-        let mut param: ArrayString<N> = ArrayString::new();
-        for &val in name {
-            let success = param.try_push(val as char);
-            if success.is_err() {
-                return Ok(param); // truncated output
-            }
-        }
-        Ok(param)
-    }
-}
-/*** ***/
 
